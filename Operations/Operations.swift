613c2c602341e9b787c58afbd57a2cc61b1cd8b2
oww10 <dntjr2209@gmail.com>
2025-08-19 18:31:08 +0900

    Initial Commit

---
+++ Operations/Operations.swift
@@ -0,0 +1,9 @@
+//
+//  Operations.swift
+//  Operations
+//
+//  Created by s s on 8/19/25.
+//
+
+import Foundation
+
---
+++ OperationsTests/OperationsTests.swift
@@ -0,0 +1,17 @@
+//
+//  OperationsTests.swift
+//  OperationsTests
+//
+//  Created by s s on 8/19/25.
+//
+
+import Testing
+@testable import Operations
+
+struct OperationsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
